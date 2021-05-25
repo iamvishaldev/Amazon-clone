c0e7d0e92f28a115850988be3369541e99306eeb
Deepak <deepak.sharma@techholding.co>
2021-05-25 23:37:25 +0530

    Added Banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
             <div className="header__nav">
                 <Link to="/login" className="header__link">
                     <div className="header__option">
-                        <span className="header__optionLineOne">Hello Qazi</span>
+                        <span className="header__optionLineOne">Hello Vishal</span>
                         <span className="header__optionLineTwo">Sign In</span>
                     </div>
                 </Link>
